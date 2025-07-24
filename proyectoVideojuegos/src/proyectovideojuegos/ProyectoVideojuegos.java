2d7b6fc9f3bf50ac688a20d1dc00af33c16b9b7d
57815971 <57815971@192.168.56.1>
2025-07-24 19:43:18 -0300

    1st commit

---
+++ proyectoVideojuegos/src/proyectovideojuegos/ProyectoVideojuegos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectovideojuegos;
+/**
+ *
+ * @author 57815971
+ */
+public class ProyectoVideojuegos {
+
+    /**
+     * @param args the command line argument
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
