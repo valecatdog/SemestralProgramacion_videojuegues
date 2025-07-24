4193292a8a9b0ff00269128c8c56f54ef23bc2af
57815971 <57815971@192.168.56.1>
2025-07-24 20:46:14 -0300

    Hice la parte logica, clase videojuego y el main. Queda ver que esté todo bien

---
+++ proyectoVideojuegos/src/logica/Main.java
@@ -0,0 +1,50 @@
+package logica;
+
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);//creo scanner para pedir datos
+        ArrayList <Videojuego> listaVideojuegos = new ArrayList<>();//array para guardar videojuegos
+        //variables para almacenar los datos que se vayan usando para crear los videojuegos
+        String nombre;
+        Year anioLanz;
+        int pegi;
+        String genero;
+        
+        for (int i = 0; i < 5; i++){//for para crear los 4 juegos que se solicitan ingresar por teclado
+            System.out.println("Ingrese el nombre del juego: ");
+            nombre = scan.nextLine();
+            
+            System.out.println("Ingrese el año de lanzamiento del juego: ");
+            anioLanz = Year.of(scan.nextInt());
+            nombre = scan.nextLine();
+            
+            System.out.println("Ingrese el PEGI del juego: ");
+            pegi = scan.nextInt();
+            nombre = scan.nextLine();
+            
+            System.out.println("Ingrese el genero del juego: ");
+            genero = scan.nextLine();
+            
+            Videojuego videojuego = new Videojuego(nombre, pegi, genero, anioLanz); //se crea una instancia
+            System.out.println(videojuego);//se muestra el videojuego que se creo
+            listaVideojuegos.add(videojuego); //se añade el nuevo videojuego al array
+        }
+        
+        System.out.println("\n");
+        
+        for (Videojuego v : listaVideojuegos){//un for each para mostrar todas las instancias creadas 
+            System.out.println(v);
+        }
+        
+        listaVideojuegos.get(0).setGeneroPorCodigo(3);//se cambia el gener de call of duty a FPS
+        System.out.println("\n");
+        System.out.println(listaVideojuegos.get(0));
+               
+    }
+    
+}
---
+++ proyectoVideojuegos/src/logica/Videojuego.java
@@ -0,0 +1,101 @@
+package logica;
+
+import java.time.Year;
+
+/*que se hace: se utiliza esta clase para crear instancias "videojuego"
+con los atributos declarados, todos con sus getters y setters corres-
+pondientes. */
+
+public class Videojuego {//atributos
+    private String nombre;
+    private int pegi;
+    private String genero; 
+    private Year anioLanzamiento; 
+    
+    public Videojuego (){ //constructor vacio
+    }
+    
+    //constructctor completo
+    public Videojuego (String nombre, int pegi, String genero, Year anioLanzamiento){
+        this.nombre = nombre;
+        this.pegi = pegi;
+        this.genero = genero;
+        if (anioLanzamiento.getValue() < 2000){
+            this.anioLanzamiento = Year.of(2000);
+        }
+        else{
+            this.anioLanzamiento = anioLanzamiento;
+        }
+    }
+
+    //getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPegi() {
+        return pegi;
+    }
+
+    public void setPegi(int pegi) {
+        this.pegi = pegi;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Year getAñoLanzamiento() {
+        return anioLanzamiento;
+    }
+    /*el setter de añolanzamiento tiene la condicion de que si el año
+    ingresado es menor a 2000, entonces el año se quedara en 2000.
+    si por el otro lado el año es mayor a 2027, el año no se actualiza,
+    o en otras palabras no permite la actualizacion
+    */
+    public void setAñoLanzamiento(Year anioLanzamiento) {
+        if (anioLanzamiento.getValue() < 2000){
+            this.anioLanzamiento = Year.of(2000);
+        }
+        else{
+            if (!(anioLanzamiento.getValue() > Year.now().plusYears(2).getValue())){
+                this.anioLanzamiento = anioLanzamiento;
+            }
+        }
+          
+    }
+    
+    public void setGeneroPorCodigo(int codigo){
+        if(codigo == 1 || codigo == 2 || codigo == 3){ //verificacion para que si se ingresa un numero invalido no se recorran 
+                                                       //todos los if
+            if (codigo == 1) {
+                this.genero = "Rol";
+            }
+            else{
+                if(codigo == 2){
+                    this.genero = "Carreras";
+                }
+                else{
+                    this.genero = "FPS";
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Videojuego{" + "nombre=" + nombre + ", pegi=" + pegi + ", genero=" + genero + ", anioLanzamiento=" + anioLanzamiento + '}';
+    }
+    
+}
+    
+    
+
