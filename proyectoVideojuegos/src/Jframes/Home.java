73b86c650f753ff477a11d9eb22e0dc46e5d1d18
57815971 <57815971@192.168.56.1>
2025-07-24 23:36:40 -0300

    Pareceria estar andadno home

--- proyectoVideojuegos/src/Jframes/Home.java
+++ proyectoVideojuegos/src/Jframes/Home.java
@@ -6,6 +6,7 @@ package Jframes;
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import Jframes.IngresarVideojuego;
 
 /**
  *
@@ -223,7 +224,7 @@ public static DefaultListModel videoJuegos;
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+   //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
